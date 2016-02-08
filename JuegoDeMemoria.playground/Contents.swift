c54b8e21f39bd087076abb32bbee8a982cd6f11c
Ray <pepperray01@gmail.com>
2016-02-08 14:46:10 -0600

    Segundo push

--- JuegoDeMemoria.playground/Contents.swift
+++ JuegoDeMemoria.playground/Contents.swift
@@ -3,4 +3,13 @@
 import UIKit
 
 
-print("Hello")
+var numeros = 0...100
+
+for numero in numeros {
+    print(numero)
+    
+    if numero % 5 == 0{
+        
+        print(String(numero) + " Bingo!!")
+    }
+}
