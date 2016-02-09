c23701152005106b3b58f3f9c82d54560fbcb2eb
Ray <pepperray01@gmail.com>
2016-02-08 18:54:51 -0600

    Juego memoria resuelto

--- JuegoDeMemoria.playground/Contents.swift
+++ JuegoDeMemoria.playground/Contents.swift
@@ -6,10 +6,20 @@ import UIKit
 var numeros = 0...100
 
 for numero in numeros {
-    print(numero)
     
-    if numero % 5 == 0{
-        
-        print(String(numero) + " Bingo!!")
+    if (numero % 5 == 0) {
+        print(String(numero) + " Bingo!!!")
     }
+
+    if numero % 2 == 0 {
+         print(String(numero) + " Número par")
+    }
+    else {
+        print(String(numero) + " Número impar")
+    }
+    
+    if numero > 29 && numero < 41 {
+        print(String(numero) + " Viva Swift!!")
+    }
+    
 }
